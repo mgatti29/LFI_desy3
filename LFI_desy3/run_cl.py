5db42da17810cd48ddef705642bc7a2c923730b5
Marco Gatti <mgatti29@sas.upenn.edu>
2023-06-05 00:33:19 -0400

    Update run_cl.py

--- LFI_desy3/run_cl.py
+++ LFI_desy3/run_cl.py
@@ -5,17 +5,54 @@ import harmony as harm
 import os
 import healpy as hp
 
-def g2k_sphere(gamma1, gamma2, mask, nside=1024, lmax=2048,nosh=True):
-    """
-    Convert shear to convergence on a sphere. In put are all healpix maps.
-    """
 
+def save_obj(name, obj):
+    """
+    Saves an object to a pickle file.
+
+    Args:
+        name (str): The name of the pickle file.
+        obj (object): The object to be saved.
+    """
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, protocol=2)
+        f.close()
+
+def load_obj(name):
+    """
+    Loads an object from a pickle file.
+
+    Args:
+        name (str): The name of the pickle file.
+
+    Returns:
+        object: The loaded object.
+    """
+    with open(name + '.pkl', 'rb') as f:
+        mute = pickle.load(f)
+        f.close()
+    return mute
+
+def g2k_sphere(gamma1, gamma2, mask, nside=1024, lmax=2048, nosh=True):
+    """
+    Converts shear to convergence on a sphere using HEALPix maps.
+
+    Args:
+        gamma1 (array): Gamma1 shear map.
+        gamma2 (array): Gamma2 shear map.
+        mask (array): Mask for the maps.
+        nside (int, optional): HEALPix nside parameter. Defaults to 1024.
+        lmax (int, optional): Maximum multipole moment. Defaults to 2048.
+        nosh (bool, optional): Flag indicating whether to remove the monopole and dipole terms. Defaults to True.
+
+    Returns:
+        tuple: E-mode convergence map, B-mode convergence map, and E-mode alms.
+    """
     gamma1_mask = gamma1 * mask
     gamma2_mask = gamma2 * mask
 
     KQU_masked_maps = [gamma1_mask, gamma1_mask, gamma2_mask]
     alms = hp.map2alm(KQU_masked_maps, lmax=lmax, pol=True)  # Spin transform!
-
 
     ell, emm = hp.Alm.getlm(lmax=lmax)
     if nosh:
@@ -23,22 +60,21 @@ def g2k_sphere(gamma1, gamma2, mask, nside=1024, lmax=2048,nosh=True):
         almsB = alms[2] * 1. * ((ell * (ell + 1.)) / ((ell + 2.) * (ell - 1))) ** 0.5
     else:
         almsE = alms[1] * 1.
-        almsB = alms[2] * 1. 
+        almsB = alms[2] * 1.
     almsE[ell == 0] = 0.0
     almsB[ell == 0] = 0.0
     almsE[ell == 1] = 0.0
     almsB[ell == 1] = 0.0
 
-
-
     almssm = [alms[0], almsE, almsB]
-
 
     kappa_map_alm = hp.alm2map(almssm[0], nside=nside, lmax=lmax, pol=False)
     E_map = hp.alm2map(almssm[1], nside=nside, lmax=lmax, pol=False)
     B_map = hp.alm2map(almssm[2], nside=nside, lmax=lmax, pol=False)
 
     return E_map, B_map, almsE
+
+
 
 import healpy as hp
 config = dict()
@@ -52,57 +88,7 @@ def doit(ii,ell_eff):
         path = root+name+'_rel{0}.npy'.format(rel)
         if not os.path.exists(path):
             cls_e1e2 = dict()
-            '''
-            for bin1 in range(1,5):
-                for bin2 in range (bin1,5):
-                    binx = '{0}_{1}'.format(bin1,bin2)
-                    #print (np.sum(mask))
-                    #print (cat[rel][bin1]['kE'])
 
-                    e1 = np.zeros(hp.nside2npix(config['nside']))
-                    e2 = np.zeros(hp.nside2npix(config['nside']))
-                    e1n = np.zeros(hp.nside2npix(config['nside']))
-                    e2n = np.zeros(hp.nside2npix(config['nside']))
-                    e1[cat[rel][bin1]['pix']] = cat[rel][bin1]['e1']
-                    e2[cat[rel][bin1]['pix']] = cat[rel][bin1]['e2']
-                    e1n[cat[rel][bin1]['pix']] = cat[rel][bin1]['e1n']
-                    e2n[cat[rel][bin1]['pix']] = cat[rel][bin1]['e2n']
-                    mask = np.in1d(np.arange(len(e1)),cat[rel][bin1]['pix'])
-                    be1 = np.zeros(hp.nside2npix(config['nside']))
-                    be2 = np.zeros(hp.nside2npix(config['nside']))
-                    be1n = np.zeros(hp.nside2npix(config['nside']))
-                    be2n = np.zeros(hp.nside2npix(config['nside']))
-                    be1[cat[rel][bin2]['pix']] = cat[rel][bin2]['e1']
-                    be2[cat[rel][bin2]['pix']] = cat[rel][bin2]['e2']
-                    be1n[cat[rel][bin2]['pix']] = cat[rel][bin2]['e1n']
-                    be2n[cat[rel][bin2]['pix']] = cat[rel][bin2]['e2n']      
-                    
-                    f,_,_   =  g2k_sphere(e1,e2, mask==mask, nside=config['nside'], lmax=config['nside']*2 ,nosh=True)
-                    fn,_,_ =  g2k_sphere(e1n,e2n,  mask==mask, nside=config['nside'], lmax=config['nside']*2 ,nosh=True)
-                    
-                    bf,_,_   =  g2k_sphere(be1,be2,  mask==mask, nside=config['nside'], lmax=config['nside']*2 ,nosh=True)
-                    bfn,_,_ =  g2k_sphere(be1n,be2n,  mask==mask, nside=config['nside'], lmax=config['nside']*2 ,nosh=True)
-                    
-            # compute power spectrum
-            #ls_ = dict()
-            #or bin1 in range(1,5):
-            #   for bin2 in range (bin1,5):
-            #       binx = '{0}_{1}'.format(bin1,bin2)
-            #       #print (np.sum(mask))
-                    f_0 = nmt.NmtField( mask==mask, [f])
-                    f_2 = nmt.NmtField( mask==mask, [bf])
-                    fn_0 = nmt.NmtField( mask==mask,[fn])
-                    fn_2 = nmt.NmtField( mask==mask,[bfn])
-
-
-                    cl_00 = nmt.compute_full_master(f_0, f_2, b)
-                    cl_00n = nmt.compute_full_master(fn_0, fn_2, b)
-                    cls_e1e2[binx] = [cl_00,cl_00n,ell_eff]
-            #       cls_[binx] = [cl_00,cl_00n,ell_eff]
-            #       
-                    
-            # compute from e1e2
-            '''
             cls_e1e2 = dict()
             for bin1 in range(1,5):
                 for bin2 in range (bin1,5):
