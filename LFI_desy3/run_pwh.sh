76675d0d1845a93a5c61e17033c68ac683f8e258
Marco Gatti <mgatti29@sas.upenn.edu>
2023-08-17 20:08:50 -0400

    Update run_pwh.sh

--- LFI_desy3/run_pwh.sh
+++ LFI_desy3/run_pwh.sh
@@ -1,13 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
 #SBATCH -A des 
 #SBATCH -C cpu 
 #SBATCH -q regular 
-#SBATCH -t 4:00:00 
-#SBATCH --nodes=20
-#SBATCH --ntasks=640
+#SBATCH -t 4:30:00 
+#SBATCH --nodes=24
+#SBATCH --ntasks=768
+
 
 module load python
-source activate py38
+source activate /global/common/software/des/mgatti/py38_clone
 cd /global/u2/m/mgatti/Mass_Mapping/peaks
 srun python run_PWH.py
+
+
+
 
