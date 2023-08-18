15dfee6ce72394145280c627a53a02e3ee920a66
Marco Gatti <mgatti29@sas.upenn.edu>
2023-08-17 21:33:41 -0400

    Update run_ST.sh

--- LFI_desy3/run_ST.sh
+++ LFI_desy3/run_ST.sh
@@ -4,11 +4,12 @@
 #SBATCH -q regular 
 #SBATCH -t 4:00:00 
 #SBATCH --nodes=20
+#SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1280
 
 module load python
 module load pytorch/1.10
-source activate py38
+source activate /global/common/software/des/mgatti/py38_clone
 pip install appdirs
 cd /global/u2/m/mgatti/Mass_Mapping/peaks
 srun python run_ST.py
