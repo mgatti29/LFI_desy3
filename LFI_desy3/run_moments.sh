ea4c841beabd8fe7b22f0d20d9edd5a6662297ba
Marco Gatti <mgatti29@sas.upenn.edu>
2023-08-17 20:49:57 -0400

    Update run_moments.sh

--- LFI_desy3/run_moments.sh
+++ LFI_desy3/run_moments.sh
@@ -8,7 +8,11 @@
 #SBATCH --ntasks=1280
 
 module load python
-source activate py38
-cd /global/u2/m/mgatti/Mass_Mapping/peaks
-srun  python run_old_moments.py 
 
+
+
+
+source activate /global/common/software/des/mgatti/py38_clone
+cd /global/u2/m/mgatti/Mass_Mapping/peaks
+srun  python run_old_moments_2.py  #run_PWH.py
+#
